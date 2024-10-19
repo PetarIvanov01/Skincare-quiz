40cf0a4f1178665b6752f68a48b08963ffffee88
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-20 00:04:27 +0300

    feat: add product card component with styling

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,68 @@
+import styles from "./ProductCard.module.css";
+
+import { useState } from "react";
+
+import FlipSvgIcon from "../SvgIcons/FlipSvgIcon";
+import LikeSvgIcon from "../SvgIcons/LikeSvgIcon";
+
+type ProductCardProps = {
+  isLiked: boolean;
+  translate: string;
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  price: string;
+  handleWishlist: (id: number) => void;
+};
+
+export default function ProductCard({
+  isLiked,
+  translate,
+  id,
+  image,
+  title,
+  description,
+  price,
+  handleWishlist,
+}: ProductCardProps) {
+  const [flipCard, setFlipCard] = useState(false);
+
+  const handleFlipping = () => {
+    setFlipCard((state) => !state);
+  };
+
+  return (
+    <div
+      style={{ translate }}
+      className={`${styles.card} ${flipCard ? styles.rotate : ""}`}
+    >
+      <div className={styles.front}>
+        <section className={styles.cardImageContainer}>
+          <img src={image} alt={title} className={styles.cardImage} />
+          <LikeSvgIcon
+            isLiked={isLiked}
+            onClick={() => handleWishlist(id)}
+            className={styles.favoriteButton}
+          />
+          <FlipSvgIcon onClick={handleFlipping} className={styles.flipButton} />
+        </section>
+        <section className={styles.titleAndPrice}>
+          <h2 className={styles.cardTitle}>{title}</h2>
+          <p className={styles.cardPrice}>${price}</p>
+        </section>
+      </div>
+      <div className={styles.back}>
+        <FlipSvgIcon
+          style={{
+            transform: "rotate(180deg)",
+          }}
+          onClick={handleFlipping}
+          className={styles.flipButton}
+        />
+        <h2 className={styles.backTitle}>Daily routine</h2>
+        <p className={styles.backDescription}>{description}</p>
+      </div>
+    </div>
+  );
+}
