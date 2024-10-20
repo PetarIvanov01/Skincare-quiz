7189ac512cf373ed8bd8bd9ecd39b7a9f24504fb
PetarIvanov01 <123456789petar45@gmail.com>
2024-10-20 03:29:08 +0300

    feat: add ProductList component with carousel and pagination functionality

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,54 @@
+import styles from "./ProductList.module.css";
+
+import useProductCarousel from "@/hooks/useProductCarousel.tsx";
+
+import ProductCarousel from "./ProductCarousel.tsx";
+import ArrowPagination from "../Pagination/Arrows/ArrowPagination.tsx";
+import DotNavigation from "../Pagination/Dots/DotNavigation.tsx";
+
+type Props = Readonly<{
+  suggestedProducts: {
+    id: number;
+    image: string;
+    title: string;
+    description: string;
+    price: string;
+  }[];
+}>;
+
+export default function ProductList({ suggestedProducts }: Props) {
+  const productsLength = suggestedProducts.length;
+  console.log(productsLength);
+
+  const {
+    productIndex,
+    productsPerSlide,
+    showNextImage,
+    showPrevImage,
+    handleDotNavigation,
+  } = useProductCarousel(productsLength);
+
+  return (
+    <div className={styles.productListWrapper}>
+      <div className={styles.productListInner}>
+        <div id="carousel" className={styles.productCarousel}>
+          <ProductCarousel
+            suggestedProducts={suggestedProducts}
+            productIndex={productIndex}
+          />
+        </div>
+
+        <ArrowPagination
+          handleNextClick={() => showNextImage()}
+          handlePrevClick={() => showPrevImage()}
+        />
+      </div>
+      <DotNavigation
+        handleDotNavigation={handleDotNavigation}
+        productsPerSlide={productsPerSlide || 1}
+        productsLength={productsLength}
+        index={productIndex}
+      />
+    </div>
+  );
+}
